6b26abee1bdccb7c58d7d5254ffe46cd66f72d03
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-04-25 14:57:38 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import crypto from 'crypto'
+import cors from 'cors'
+import mysql from 'mysql'
+
+const app = express()
+const PORT = 7000
+
+app.use(express.json())
+app.use(cors())
+
+dotenv.config()
+
+app.get("/", (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
