68b275df1959c4c61be4e3874a27df2e0bc09525
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-04-27 11:24:24 +0200

    added register page

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -6,18 +6,35 @@ import earth from "./assets/Earth-Transparent.png"
 
 export default function Root() {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [login, setLogin] = useState(false)
+    const [register, setRegister] = useState(false)
 
     return (
         <div className="h-screen bg-gradient-to-tl from-sky-950 via-violet-900 to-blue-950 text-white">
-            {!isLoggedIn ? (
+            {register ? (
+                <div className="flex flex-col items-center">
+                    <img className="w-3/4 self-center object-contain max-w-lg" src={earth} alt="" />
+                    <div className="flex flex-col items-center">
+                        <label htmlFor="username">Användarnamn: </label>
+                        <input className="w-56" id="username" type="text" />
+                        <label htmlFor="password">Lösenord</label>
+                        <input className="w-56" id="password" type="text" />
+                        <button>Registra dig</button>
+                    </div>
+                </div>
+            ) : login ? (
+                <div>
+                    login
+                </div>
+            ) : !isLoggedIn ? (
                 <div className="flex flex-col justify-between h-screen">
                     <button className="h-8 w-8 p-0 self-end m-2">
                         <img className="h-full w-full object-cover rounded-full" src={sweden} alt="Swedish flag" />
                     </button>
                     <img className="w-3/4 self-center object-contain" src={earth} alt="" />
                     <div className="flex flex-col gap-3 items-center mb-24">
-                        <button className="bg-gray-200 text-black rounded-full px-3 py-1 w-fit" onClick={() => setIsLoggedIn(true)}>Logga in</button>
-                        <button className="bg-gray-200 text-black rounded-full px-3 py-1 w-fit">Registrera dig</button>
+                        <button className="bg-gray-200 text-black rounded-full px-3 py-1 w-fit" onClick={() => setLogin(true)}>Logga in</button>
+                        <button className="bg-gray-200 text-black rounded-full px-3 py-1 w-fit" onClick={() => setRegister(true)}>Registrera dig</button>
                     </div>
                 </div>
             ) : (
