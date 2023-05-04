24b8197e634213042b4e532f41dccb90e9ffe130
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-04 12:56:26 +0200

    Revert " did login and register layout"
    This reverts commit d5df40c06a3d8b4e84f845bb2381926506c3db75.

---
+++ frontend/src/Root.jsx
@@ -0,0 +1,29 @@
+import { Link, Outlet } from "react-router-dom";
+import sweden from "./assets/sweden.png"
+import { useIsLoggedIn } from "./redux/isLoggedIn";
+import { HiMenu } from "react-icons/hi"
+
+
+export default function Root() {
+    const isLoggedIn = useIsLoggedIn()
+
+    return (
+        <div className="h-screen bg-gradient-to-tl from-sky-950 via-violet-900 to-blue-950 text-white">
+            <header className="flex justify-between">
+                <div className="m-2">Earth Hero</div>
+                <nav className="flex items-center">
+                    <button className="h-8 w-8 p-0 self-end m-1 border border-transparent hover:border-white rounded-full overflow-hidden">
+                        <img className="h-full w-full object-cover" src={sweden} alt="Swedish flag" />
+                    </button>
+                    {isLoggedIn && <button className="m-2"><HiMenu size={'24px'}/></button>}
+                </nav>
+            </header>
+            <main className="">
+                <Outlet />
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 /* import App from './App.jsx' */
-import Root from './pages/Root.jsx'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import User from './pages/User.jsx'
+import Root from './Root.jsx'
+import Home from './Home.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Provider } from 'hooks-for-redux'
@@ -18,18 +15,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />
-      },
-      {
-        path: "/register",
-        element: <Register />
-      },
-      {
-        path: "/login",
-        element: <Login />
-      },
-      {
-        path: "/user",
-        element: <User />
       }
     ]
   }
