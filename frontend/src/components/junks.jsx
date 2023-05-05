f5d113ac596cf7c986eb77ece7e546fd29aea276
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-05 09:11:23 +0200

    pictures for components

--- frontend/src/components/junks.jsx
+++ frontend/src/components/junks.jsx
@@ -1,30 +1,12 @@
-const styles = {
-  color: {
-    primary: "bg-pink-500 hover:bg-pink-300",
-    secondary: "bg-blue-500 hover:bg-blue-300",
-  },
+import React from "react";
+import bottom from "../assets/bottom.svg";
 
-  size: {
-    small: "text-sm px-4 py-2",
-    large: "text-lg px-8 py-2",
-  },
-
-  disabled: "bg-gray-500 cursor-not-allowed",
-};
-
-export default function Button(props) {
-  const { title, color, size, disabled } = props;
-
-  const colorClass = styles.color[color];
-  const sizeClass = styles.size[size];
-  const disabledClass = disabled ? styles.disabled : "";
-
+function App() {
   return (
-    <button
-      disabled={disabled}
-      className={`rounded-lg ${colorClass} ${sizeClass} ${disabledClass}`}
-    >
-      {title}
-    </button>
+    <div>
+      <img className="w-3/4 self-center object-contain" src={bottom} alt="" />
+    </div>
   );
 }
+
+export default App;
