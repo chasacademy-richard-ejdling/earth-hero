31092caa7fc88041d1deaa6013e4650ec38cad73
Richard Ejdling <richard.ejdling@chasacademy.se>
2023-05-10 14:18:01 +0200

    added modal component + policy modal

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,38 @@
+/* 
+To use this component you need to create a useState "const [<stateName>, <setStateName>] = useState(false)"
+
+Parameters:
+    state: The <stateName> boolean from the useState that determines wether modal is open or not. Reccommended to be false by default.
+    setState: Callback function to <setStateName> in the useState. Used to change the state to open and close the modal.
+    children: The main content of the modal. Sould not include the misty-white background or the "X" to close the modal,
+              they are allready in place.
+*/
+
+
+import { useEffect } from "react"
+import close from "../assets/Close.svg"
+import bodyScroll from "body-scroll-toggle"
+
+export default function Modal({ state, setState, children }) {
+
+    useEffect(() => {
+        if (state === true) {
+            bodyScroll.disable()
+        } else {
+            bodyScroll.enable()
+        }
+    }, [state])
+
+    return (
+        <div>
+            {
+                state && (
+                    <div id='close' className='flex justify-end bg-white/70 fixed top-0 right-0 z-10 h-screen w-screen' onClick={(e) => e.target.id === 'close' && setState(false)}>
+                        <button className='fixed top-0 right-0 h m-2' onClick={() => setState(false)}><img src={close} /></button>
+                        {children}
+                    </div>
+                )
+            }
+        </div>
+    )
+}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,17 @@
 import sweden from "../assets/sweden.png"
 import logo from "../assets/logo.svg"
 import { useState } from "react"
+import { RxCross2 } from "react-icons/rx"
+import Modal from "../components/Modal"
 
 export default function Register() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [policyChecked, setPolicyChecked] = useState(false)
+    const [readingPolicy, setReadingPolicy] = useState(false)
 
-  console.log(policyChecked);
+    console.log(policyChecked);
 
     return (
         <div className="flex flex-col items-center">
@@ -31,10 +34,19 @@ export default function Register() {
                 </div>
                 <label htmlFor="policy" className="block">
                     <input type="checkbox" id="policy" className="align-middle relative bottom-[1px] mr-1" onClick={() => setPolicyChecked(!policyChecked)} />
-                    Godkänn policy
+                    Godkänn <button onClick={() => setReadingPolicy(true)} className="underline text-blue-600">policy</button>
                 </label>
                 <button disabled={!username || !email || !password || !policyChecked} className="self-end bg-gray-200 enabled:hover:bg-gray-300 disabled:opacity-70 text-black rounded-full px-3 py-1 w-fit">Registra dig</button>
             </div>
+            <Modal state={readingPolicy} setState={setReadingPolicy} children={(
+                <div className='flex flex-col w-3/4 m-auto h-fit bg-sky-300 p-4 rounded-lg max-h-[75%] overflow-scroll'>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aut quasi aliquam consequatur earum nulla suscipit maiores vitae nostrum minima eveniet aperiam, mollitia quisquam molestias deserunt eaque maxime numquam exercitationem.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque alias aspernatur ratione in eaque ut sed commodi nam error distinctio vitae eveniet doloribus, quia possimus esse ipsa, assumenda eos aperiam!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod molestias ad temporibus aliquid beatae ducimus dolores velit. Inventore officiis, est, beatae velit rem consequatur, labore iure aperiam aliquid aut magni?
+                    </p>
+                </div>
+            )} />
         </div>
-  );
+    );
 }
