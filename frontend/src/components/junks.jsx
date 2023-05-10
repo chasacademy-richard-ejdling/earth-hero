7217bf8f49a9eed7169b45e8061d8b5d6665d0a0
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-10 09:35:57 +0200

    start coding

--- frontend/src/components/junks.jsx
+++ frontend/src/components/junks.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import bottom from "../assets/bottom.svg";
+import plast from "./assets/plast.svg";
+import kompost from "./assets/kompost.svg";
+import elektronik from "./assets/elektronik.svg";
+import tidning from "./assets/tidning.svg";
+import glas from "./assets/glas.svg";
+import metall from "./assets/metall.svg";
+import tunna from "./assets/tunna.svg";
+import lock from "./assets/lock.svg";
+
+const imgUrl = new Array(
+  plast,
+  kompost,
+  elektronik,
+  tidning,
+  glas,
+  metall,
+  tunna,
+  lock
+);
 
 function App() {
   return (
-    <div>
-      <img className="w-3/4 self-center object-contain" src={bottom} alt="" />
-    </div>
+    <>
+      {imgUrl.map((s, i) => (
+        <div key={i} className="flex column items-baseline mb-8">
+          <h2 style={{ fontSize: 30 }}>text</h2>
+          <img src={`.${s}`} alt="" />
+        </div>
+      ))}
+    </>
   );
 }
 
