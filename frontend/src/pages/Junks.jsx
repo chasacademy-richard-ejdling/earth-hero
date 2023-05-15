025bdbf1c91714df9838627b74df42f9bb335aac
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-15 08:31:52 +0200

    did som codig

--- frontend/src/pages/Junks.jsx
+++ frontend/src/pages/Junks.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import plast from "../assets/plast.svg";
-import kompost from "../assets/kompost.svg";
-import elektronik from "../assets/elektronik.svg";
-import tidning from "../assets/tidning.svg";
-import glas from "../assets/glas.svg";
-import metall from "../assets/metall.svg";
-import tunna from "../assets/tunna.svg";
-import lock from "../assets/lock.svg";
+import plast from "./assets/plast.svg";
+import kompost from "./assets/kompost.svg";
+import elektronik from "./assets/elektronik.svg";
+import tidning from "./assets/tidning.svg";
+import glas from "./assets/glas.svg";
+import metall from "./assets/metall.svg";
+import tunna from "./assets/tunna.svg";
+import lock from "./assets/lock.svg";
 
 const imgUrl = new Array(plast, kompost, elektronik, tidning, glas, metall);
 
@@ -15,17 +15,21 @@ function Junks() {
 
   return (
     <>
-      <img
-        onDragOver={() => setSrc(lock)}
-        onDragLeave={() => setSrc(tunna)}
-        src={src}
-        alt=""
-      />
-      {imgUrl.map((src, i) => (
-        <React.Fragment key={i}>
-          <img draggable="true" id={i} src={src} alt="" />
-        </React.Fragment>
-      ))}
+      <div className="flex flex-col items-center">
+        <img
+          onDragOver={() => setSrc(lock)}
+          onDragLeave={() => setSrc(tunna)}
+          src={src}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        {imgUrl.map((src, i) => (
+          <React.Fragment key={i}>
+            <img draggable="true" id={i} src={src} alt="" />
+          </React.Fragment>
+        ))}
+      </div>
     </>
   );
 }
