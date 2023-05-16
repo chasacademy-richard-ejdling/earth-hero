bc0542b164edca430acc2bd4dda6abcab89bfd20
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-16 13:33:22 +0200

    Co-authored-by: Robin Lundström <rroobin98@users.noreply.github.com> Co-authored-by: Richard Ejdling <chasacademy-richard-ejdling@users.noreply.github.com> Co-authored-by: Farida Ouro Möllah <Farida1084@users.noreply.github.com> Co-authored-by: iida hellström <chasacademy-iida-hellstrom@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-import express from "express"
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
-import crypto from 'crypto'
-import cors from 'cors'
-import mysql from 'mysql'
-import cookieParser from "cookie-parser"
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import crypto from "crypto";
+import cors from "cors";
+import mysql from "mysql";
+import cookieParser from "cookie-parser";
 
-const app = express()
-const PORT = 7000
+const app = express();
+const PORT = 7000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-dotenv.config()
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send('Hello World!')
-})
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT)
-})
+  console.log("Server is running on port " + PORT);
+});
