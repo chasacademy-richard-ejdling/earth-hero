a6d37fc42da1bcc9d835b38c536a8a3c7537074a
Camilla Sundbom <camilla.sundbom@outlook.com>
2023-05-16 10:45:46 +0200

    deleted wrong pictures and added correct ones

--- frontend/src/pages/Junks.jsx
+++ frontend/src/pages/Junks.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import plast from "../assets/plast.svg";
-import kompost from "../assets/kompost.svg";
+import bottle from "../assets/bottle.svg";
+import chipsbag from "../assets/chipsbag.svg";
 import elektronik from "../assets/elektronik.svg";
-import tidning from "../assets/tidning.svg";
 import glas from "../assets/glas.svg";
+import kartong from "../assets/kartong.svg";
+import kompost from "../assets/kompost.svg";
 import metall from "../assets/metall.svg";
+import tidning from "../assets/tidning.svg";
 import tunna from "../assets/tunna.svg";
 import lock from "../assets/lock.svg";
 
-const imgUrl = new Array(plast, kompost, elektronik, tidning, glas, metall);
+const imgUrl = new Array(
+  bottle,
+  kompost,
+  elektronik,
+  tidning,
+  glas,
+  metall,
+  kartong,
+  chipsbag
+);
 
 function Junks() {
   const [src, setSrc] = React.useState(tunna);
 
   return (
     <>
-      <div className="flex flex-col items-center">
+      <div className="flex justify-center">
         <img
           onDragOver={() => setSrc(lock)}
           onDragLeave={() => setSrc(tunna)}
@@ -23,7 +34,7 @@ function Junks() {
           alt=""
         />
       </div>
-      <div className="flex flex-col items-center">
+      <div className="grid grid-cols-4 gap-10 place-content-center ">
         {imgUrl.map((src, i) => (
           <React.Fragment key={i}>
             <img draggable="true" id={i} src={src} alt="" />
